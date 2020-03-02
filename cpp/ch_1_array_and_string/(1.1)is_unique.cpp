41ee1a0aa1da25e499894490332099cf0958d8ce
itsvinayak <itssvinayak@gmail.com>
2020-03-02 20:57:47 +0530

    cpp

---
+++ cpp/ch_1_array_and_string/(1.1)is_unique.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int is_unique(string s){
+    int len = 
+
+
+int main(){
+    int number,ans;
+    cin>>number;
+
+    ans = is_unique(number);
+
+    cout<<ans<<endl;
+}
