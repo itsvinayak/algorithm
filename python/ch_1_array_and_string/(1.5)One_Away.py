2360ceec0dd7e304e95c091c664d7539dcf85eb8
itsvinayak <itssvinayak@gmail.com>
2020-03-09 10:19:29 +0530

    one away

--- python/ch_1_array_and_string/(1.5)One_Away.py
+++ python/ch_1_array_and_string/(1.5)One_Away.py
@@ -1,26 +1,25 @@
+
+
 # There are three types of edits that can be performed on strings: insert a character,
 # remove a character, or replace a character. Given two strings, write a function to check if they are
 # one edit (or zero edits) away.
 
 
 def oneAway(s1,s2):
-    if len(s1) == len(s2):
-        return oneEditReplace(s1,s2)
-    elif abs(len(s1)-len(s2)) == 1 :
-        return oneEditInsert(s1,s2)
-    return 0
+    c=0
+    for i in s1:
+        if i not in s2:
+            c+=1
+    if c>1:
+        return False
+    else:
+        return True
 
 
-# function to see only one element is different
-def oneEditReplace(s1,s2):
-    foundDifferent = False
-    for i in range(len(s1)):
-        if s1[i] != s2[i] :
-            if foundDifferent:
-                return 0
-            foundDifferent = True
-    return 1
+
+if __name__ == "__main__":
+    s1,s2 = map(str,input().split())
+    print(oneAway(s1,s2))
 
 
-def oneEditInsert(s1,s2):
-    
+ 
