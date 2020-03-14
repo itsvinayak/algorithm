53c552de3c053ed9941004b33e6aaf11142b387f
itsvinayak <itssvinayak@gmail.com>
2020-03-14 16:30:52 +0530

    16.1

--- python/Moderate/(16.1)Number_swapper.py
+++ python/Moderate/(16.1)Number_swapper.py
@@ -9,7 +9,17 @@ def swap(a,b):
 
     return(str(a)+' '+str(b))
 
+def swapUsingXor(a,b):
+    a = a^b
+    b = a^b
+    a = a^b
+
+    return(str(a)+' '+str(b))
+
+
+
 
 if __name__ == "__main__":
     a,b = map(int,input().split())
     print(swap(a,b))
+    print(swapUsingXor(a,b))
