46613131135938087c93b18654ec65afe1504729
itsvinayak <itssvinayak@gmail.com>
2020-03-14 16:25:56 +0530

    16.2

---
+++ python/Moderate/(16.2)Word_frequencies.py
@@ -0,0 +1,17 @@
+# Design a method to find the frequency of occurrences of any given word
+# in a book. what if we were running this algorithm multiple times ?
+
+def wordFrequencies(string):
+    HashMap = {}
+    string = list(string.split())
+    for i in string:
+        if i in HashMap:
+            HashMap[i] += 1
+        else:
+            HashMap[i] = 1
+
+    return HashMap
+
+if __name__ == '__main__':
+    string = "i am vinayak a student at iert vinayak is a nice name"
+    print(wordFrequencies(string))
