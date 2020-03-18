ab54ead238a13b91dfb66f2e626050ced10e55f9
vinayak <itssvinayak@gmail.com>
2020-03-18 11:23:13 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,13 @@
 import re
 import os
 file = open('README.md',"w")
-file.writelines('<p text-align="center"><h1>BOOKS</h1></p>')
+file.writelines('<p text-align="center"><h1>cracking the coding interview</h1></p>')
 file.writelines('<hr/>')
-file.writelines('some book related to computer Science')
+file.writeline('<center><img src="img/img.png" alt="image" /></center>')
+file.writelines('<hr/>')
+file.writelines('solution for book "cracking the coding interview')
+file.writelines('<h2> Index </h2>')
+
 for list in os.walk('.',topdown=True):
     if len(re.findall('\.',list[0])) == 1 and os.path.basename(list[0]) != '.':
         print(list[0])
