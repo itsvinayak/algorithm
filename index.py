18d096c9ab932d5c8cad9792cd37822fb119225b
vinayak <itssvinayak@gmail.com>
2020-03-18 11:59:44 +0530

    Update index.py

--- index.py
+++ index.py
@@ -2,11 +2,11 @@ import re
 import os
 file = open('README.md',"w")
 file.writelines('<p text-align="center"><h1>cracking the coding interview</h1></p>')
-file.writelines('<hr/>')
+
 file.writelines('<center><img src="img/img.png" alt="image" /></center>')
 file.writelines('<hr/>')
 file.writelines('solution for book "cracking the coding interview')
-file.writelines('<h2> Index </h2>')
+file.writelines('<p text-align="center"><h2> Index </h2></p>')
 
 for list in os.walk('.',topdown=True):
     if len(re.findall('\.',list[0])) == 1 and os.path.basename(list[0]) not in ['.','img','book','python']:
