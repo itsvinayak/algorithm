626c234f29579771db309337792f5b0616d6b1bf
itsvinayak <itssvinayak@gmail.com>
2020-03-18 11:50:15 +0530

    script

--- index.py
+++ index.py
@@ -9,13 +9,13 @@ file.writelines('solution for book "cracking the coding interview')
 file.writelines('<h2> Index </h2>')
 
 for list in os.walk('.',topdown=True):
-    if len(re.findall('\.',list[0])) == 1 and os.path.basename(list[0]) != '.':
+    if len(re.findall('\.',list[0])) == 1 and os.path.basename(list[0]) not in ['.','img','book','python']:
         print(list[0])
-        file.writelines('<h2>' + ' > '.join(list[0].split('/')[1:]) +'</h2>')
+        file.writelines('<h2>' + ' --> '.join(list[0].split('/')[1:]) +'</h2>')
         file.writelines('<br/>')
         file.writelines("<ul>")
         for line in list[2]:
             file.writelines(' <li><a href=" ' + line +'" >'+ os.path.basename(line) + '</a></li>')
         file.writelines("</ul>")
-    file.writelines('<hr/>')
+        file.writelines('<hr/>')
 file.close()
