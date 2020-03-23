83bad661d96de5dcbba620120173f6daae6f895f
itsvinayak <itssvinayak@gmail.com>
2020-03-23 11:31:16 +0530

    index

--- index.py
+++ index.py
@@ -10,12 +10,11 @@ file.writelines('<p text-align="center"><h2> Index </h2></p>')
 
 for list in os.walk('.',topdown=True):
     if len(re.findall('\.',list[0])) == 1 and os.path.basename(list[0]) not in ['.','img','book','python']:
-        print(list[0])
         file.writelines('<h2>' + ' --> '.join(list[0].split('/')[1:]) +'</h2>')
         file.writelines('<br/>')
         file.writelines("<ul>")
         for dir in list[2]:
-            file.writelines(' <li><a href=" .' + list[0][1:] +' " >'+ os.path.basename(dir) + '</a></li>')
+            file.writelines(' <li><a href="' + '/'.join(list[0].split('/')[1:])+'/'+dir +'" >'+ os.path.basename(dir) + '</a></li>')
         file.writelines("</ul>")
         file.writelines('<br/>')
 file.close()
