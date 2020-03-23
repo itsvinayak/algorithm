f304b91a2c10b19dc7d69b78ffb186f476be2b54
itsvinayak <itssvinayak@gmail.com>
2020-03-23 10:58:21 +0530

    recusion

---
+++ python/ch_8_recursion_and_dynaminc_programming/(8.4)power_set.py
@@ -0,0 +1,38 @@
+ #   {1,2} == total no. of PowerSet is 2**n, where n in no. of element
+ #
+ #     {}
+ #      |
+ #     / \
+ #    {1} {}    |
+ #     |   |
+ #    / \   \
+ #   {1,2}  {2}
+ #
+ #
+
+#
+#
+# def PowerSet(s):
+#     subset = []
+#     helper(list(s),subset,0)
+#
+# def helper(s,subset,i):
+#     if i == len(s)-1:
+#         print(subset)
+#     else:
+#         helper(s,subset,i+1)
+#         print('--> ',i)
+#         subset.append(s[i])
+#         helper(s,subset,i+1)
+
+
+def PowerSet(s):
+    subset = [{}]
+    s = lsit(s)
+    for i in range(len(s)):
+        for j in range(i):
+            subset.append(s[j])
+
+
+if __name__ == '__main__':
+    PowerSet({1,2})
