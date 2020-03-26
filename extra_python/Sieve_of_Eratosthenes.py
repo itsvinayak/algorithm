4fe92587c9d7d936d97b4478a840a305443c8530
itsvinayak <itssvinayak@gmail.com>
2020-03-26 09:56:29 +0530

    black

---
+++ extra_python/Sieve_of_Eratosthenes.py
@@ -0,0 +1,22 @@
+def prime_sum(n):
+    l = [0 for i in range(n + 1)]
+
+    l[0] = 1
+    l[1] = 1
+    for i in range(2, int(n ** 0.5) + 1):
+        if l[i] == 0:
+            for j in range(i * i, n + 1, i):
+                l[j] = 1
+    s = 0
+    for i in range(n):
+        if l[i] == 0:
+            s += i
+    print(s)
+
+
+def main():
+    prime_sum(2000000)
+
+
+if __name__ == "__main__":
+    main()
