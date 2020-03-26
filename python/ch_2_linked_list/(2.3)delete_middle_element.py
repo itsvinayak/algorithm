933371532ea61c42940d27ae66a32cb1b4dae265
itsvinayak <itssvinayak@gmail.com>
2020-03-26 10:18:51 +0530

    ll

--- python/ch_2_linked_list/(2.3)delete_middle_element.py
+++ python/ch_2_linked_list/(2.3)delete_middle_element.py
@@ -23,22 +23,38 @@ class LinkedList:
             print(node.val)
             ll.printLinkedList(node.next)
 
+# Deletes middle node
     def deleteMiddleElement(self):
+        #Base cases
+        if self.head == None:
+            return None
+        if self.head.next == None:
+            self.head = None
+            return None
+        # Initialize slow and fast pointers to reach
+        fast = self.head
         slow = self.head
-        fast = self.head.next
-
-        while fast.next.next:
+        # Find the middle and previous of middle.
+        while fast != None and fast.next != None:
+            fast = fast.next.next
+            prev = slow
             slow = slow.next
+        # skiping middle element
+        prev.next = slow.next
 
-        print(fast.val)
 
-
+#Drier program to test above function
 if __name__ == "__main__":
     ll = LinkedList()
     head = ll.makeNode(1)
     ll.makeNode(2)
-    ll.makeNode(1)
+    ll.makeNode(3)
     ll.makeNode(4)
+    ll.makeNode(5)
+    print("Gven Linked List ")
     ll.printLinkedList(head)
     ll.deleteMiddleElement()
+    print('Linked List after deletion of middle')
     ll.printLinkedList(head)
+
+# by vinayak
