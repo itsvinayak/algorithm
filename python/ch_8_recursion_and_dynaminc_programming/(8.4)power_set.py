eb825e1d5980857a6dafceddf83fd9b0ade621b2
itsvinayak <itssvinayak@gmail.com>
2020-03-29 22:47:15 +0530

    power seet

--- python/ch_8_recursion_and_dynaminc_programming/(8.4)power_set.py
+++ python/ch_8_recursion_and_dynaminc_programming/(8.4)power_set.py
@@ -1,27 +1,38 @@
-#   {1,2} == total no. of PowerSet is 2**n, where n in no. of element
+#    question is to find all PowerSet for a given set
+#
+#  {1,2} == total no. of PowerSet is 2**n, where n in no. of element
+#
+##########################################
+#   [1, 2, 3]
+#      |
+#   [1, 2, 3]
+#   [1, 2]
+#   [1, 3]
+#   [1]
+#   [2, 3]
+#   [2]
+#   [3]
+#   []
+#
+##########################################
 #
 #     {}
 #      |
 #     / \
-#    {1} {}    |
+#    {1} {}
 #     |   |
 #    / \   \
 #   {1,2}  {2}
 #
-#
-
+##########################################
 
 def PowerSet(s,subset,index):
-
       if index == len(s):
         print(subset)
         return
-      print(s[index])
-      print("-> ", index)
-      PowerSet(s, subset.append(s[index-1]), index + 1)
+      PowerSet(s, subset + [s[index]], index + 1)
       PowerSet(s, subset, index + 1)
 
 
-
 if __name__ == "__main__":
-    PowerSet([1,2,3],[],0)
+    PowerSet([1,2],[],0)
