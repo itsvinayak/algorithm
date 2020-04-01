9b91ea1c8ccdbefc3687917b434d00786ad6f507
itsvinayak <itssvinayak@gmail.com>
2020-04-01 11:11:49 +0530

    ch 1

---
+++ python/ch_2_linked_list/(2.4)Partition.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def makeNode(self,data):
+        if self.head is None:
+            self.head = Node(data)
+            return self.head
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            curr.next = Node(data)
+    def printLinkedList(self, node):
+        if node is not None:
+            print(node.val)
+            ll.printLinkedList(node.next) 
