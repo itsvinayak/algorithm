6b382be6891f8a260110f1b7f2974b70861c3df9
itsvinayak <itssvinayak@gmail.com>
2020-04-01 13:33:22 +0530

    ch 8

---
+++ extra_python/kadanes.py
@@ -0,0 +1,15 @@
+def findMaxSubarraySum(l):
+    maxSum = l[0]
+    tempSum = l[0]
+
+    for i in range(len(l)):
+        tempSum = max(l[i],tempSum+l[i])
+
+        if maxSum < tempSum:
+            maxSum = tempSum
+    
+    return maxSum
+
+
+if __name__ == "__main__":
+    print(findMaxSubarraySum([-2,3,4,2]))
