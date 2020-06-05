61d7c1102c068be1dea10cb0da8eecd04d2c737c
itsvinayak <itssvinayak@gmail.com>
2020-06-06 03:33:04 +0530

    d

--- code/dynamic_programming/MatrixChainMultiplication.py
+++ code/dynamic_programming/MatrixChainMultiplication.py
@@ -1,6 +1,9 @@
 import sys
 
 
+memo = [[None for _ in range(1001)] for _ in range(1001)]
+
+
 def MatrixChainMultiplicationRec(array, i, j):
     if i == j:
         return 0
@@ -17,9 +20,48 @@ def MatrixChainMultiplicationRec(array, i, j):
     return minn
 
 
+def MatrixChainMultiplicationMemo(array, i, j):
+    if i == j:
+        return 0
+    if memo[i][j] is not None:
+        return memo[i][j]
+
+    minn = sys.maxsize
+
+    for k in range(i, j):
+        count = (
+            MatrixChainMultiplicationMemo(array, i, k)
+            + MatrixChainMultiplicationMemo(array, k + 1, j)
+            + (array[i - 1] * array[k] * array[j])
+        )
+        minn = min(minn, count)
+    memo[i][j] = minn
+    return memo[i][j]
+
+
+def MatrixChainMultiplicationDp(array, n):
+    """ n is length of array """
+    dp = [[None for _ in range(n + 1)] for _ in range(n + 1)]
+    for i in range(n + 1):
+        for j in range(n + 1):
+            if i == j:
+                dp[i][j] = 0
+            else:
+                dp[i][j] = sys.maxsize
+                for k in range(i + 1, j):
+                    count = (
+                        dp[i][k] + dp[k + 1][j] + (array[i - 1] * array[k] * array[j])
+                    )
+                    dp[i][j] = min(count, dp[i][j])
+
+    return dp[n][n]
+
+
 def main():
     array = [1, 2, 3, 4, 3]
-    print(MatrixChainMultiplicationRec(array, 1, len(array)-1))
+    print(MatrixChainMultiplicationRec(array, 1, len(array) - 1))
+    print(MatrixChainMultiplicationMemo(array, 1, len(array) - 1))
+    print(MatrixChainMultiplicationDp(array, len(array)))
 
 
 if __name__ == "__main__":
