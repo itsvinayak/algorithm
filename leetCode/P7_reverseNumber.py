b09274cf55e90c93016228eb0fa4c7d790bb8465
itsvinayak <itssvinayak@gmail.com>
2020-06-14 17:31:03 +0530

    stack

--- leetCode/P7_reverseNumber.py
+++ leetCode/P7_reverseNumber.py
@@ -1,11 +1,11 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x>=0:
+        if x >= 0:
             ans = int(str(x)[::-1])
         else:
             ans = -1 * int(str(abs(x))[::-1])
 
-        if ans >= 2**31 or ans <= -2**31 :
+        if ans >= 2 ** 31 or ans <= -2 ** 31:
             return 0
         else:
             return ans
@@ -13,22 +13,22 @@ class Solution:
 
 ######################################################
 
+
 class Solution:
     def reverse(self, x: int) -> int:
         res = 0
-        neg = 'N'
+        neg = "N"
         if x != abs(x):
-            x = x*-1
-            neg = 'Y'
+            x = x * -1
+            neg = "Y"
 
-        while(x!=0):
-            res = res * 10 + x%10
-            x = int(x/10)
+        while x != 0:
+            res = res * 10 + x % 10
+            x = int(x / 10)
 
-        ans = int(res) * -1 if neg == 'Y' else int(res)
+        ans = int(res) * -1 if neg == "Y" else int(res)
 
-        if ans >= 2**31 or ans <= -2**31 :
+        if ans >= 2 ** 31 or ans <= -2 ** 31:
             return 0
         else:
             return ans
-
